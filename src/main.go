9a28c28f597db619a2e38508f1d8b78111d53ab5
ah-naf <sheikhahnafshifat@gmail.com>
2024-12-23 21:39:13 +0600

    add redirect code

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 
 	"www.github.com/ah-naf/urlshort/utils"
 )
@@ -14,5 +15,31 @@ func main() {
 
 	configs := utils.ParseFile(*format, *path)
 
-	fmt.Println(configs)
+	mapPathToUrl := make(map[string]string)
+	for _, config := range configs {
+		mapPathToUrl[config.Path] = config.URL
+	}
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if val, ok := mapPathToUrl[path]; ok {
+			http.Redirect(w, r, val, http.StatusMovedPermanently)
+			return
+		}
+
+		http.NotFound(w, r)
+	})
+
+	fmt.Println("Server is listening on http://localhost:8080")
+
+	// Start the server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 }
